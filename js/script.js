d885b86df156f238c0221486865293e7bc21cb65
Alejandro Aragon <aragondru155@gmail.com>
2022-04-13 00:33:15 -0600

    parallax 50%

--- js/script.js
+++ js/script.js
@@ -1,36 +1,13 @@
-$(function() {
-    $(".toggle").on("click", function() {
-        if ($(".item").hasClass("active")) {
-            $(".item").removeClass("active");
-            $(this).find("a").html("<i class='fas fa-bars'></i>");
-        } else {
-            $(".item").addClass("active");
-            $(this).find("a").html("<i class='fas fa-times'></i>");
-        }
-    });
-});
-
-/**
- * Tab vertical
- */
- function openCity(evt, seccion) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    console.log(tabcontent.length)
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(seccion).style.display = "block";
-    evt.currentTarget.className += " active";
+function parallax_height() {
+    var scroll_top = $(this).scrollTop();/**scrollTop https://developer.mozilla.org/es/docs/Web/API/Element/scrollTop */
+    var header_height = $(".sample-header-section").outerHeight();/** OUTERHEIGHT https://developer.mozilla.org/es/docs/Web/API/Window/outerHeight */
+    $(".sample-section").css({ "margin-top": header_height });
+    $(".sample-header").css({ height: (header_height - scroll_top)+(header_height - scroll_top)*0.1 });
   }
+  parallax_height();
+  $(window).scroll(function() {
+    parallax_height();
+  });
+  $(window).resize(function() {
+    parallax_height();
+  });
